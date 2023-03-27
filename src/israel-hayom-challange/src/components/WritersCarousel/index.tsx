eab1b3d588cd8c6553239a1567872584a897c7f2
Boydem <noyemdahan@gmail.com>
2023-03-27 19:29:13 +0300

    fixing component's styles

--- src/israel-hayom-challange/src/components/WritersCarousel/index.tsx
+++ src/israel-hayom-challange/src/components/WritersCarousel/index.tsx
@@ -1,5 +1,7 @@
 import { Writer } from '@/pages'
 import { NextPage, GetServerSideProps } from 'next'
+import Image from 'next/image'
+import styles from './WritersCarousel.module.scss'
 
 interface Props {
     writers: Writer[]
@@ -9,28 +11,34 @@ const WritersCarousel: NextPage<Props> = ({ writers }) => {
     console.log('writers:', writers)
 
     return (
-        <div className='writers-carousel'>
-            <section className='header'>
+        <div className={styles['writers-carousel']}>
+            <section className={`${styles['carousel-header']} flex align-center justify-between`}>
                 <h1>נבחרת הכתבים</h1>
-                <button className='btn-next-page'>{'<<'}</button>
+                <button>{'>>'}</button>
             </section>
-            <section className='carousel-body'>
-                <ul className='writers-list clean-list'>
+            <main>
+                <ul className={`${styles['writers-list']} clean-list`}>
                     {writers.map(writer => (
-                        <li key={writer.id} className='writer-preview'>
-                            <div className='writer'>
-                                {writer.img_url}
-                                <div className='name'>{writer.name}</div>
+                        <li key={writer.id} className={styles['writer-preview']}>
+                            <div className={styles['writer']}>
+                                <Image
+                                    width={184}
+                                    height={223}
+                                    className={styles['writer-img']}
+                                    src={writer.img_url}
+                                    alt='writer'
+                                />
+                                <h2>{writer.name}</h2>
                             </div>
-                            <ul className='posts-list-preview clean-list'>
+                            <ul className={`${styles['posts-list']} flex column clean-list`}>
                                 {writer.posts.map(post => (
-                                    <li key={post.id}>{post.content}</li>
+                                    <li key={post.id}>{'לורם איפסום דולור סיט אמט, קנסטרקטר עלית גל'}</li>
                                 ))}
                             </ul>
                         </li>
                     ))}
                 </ul>
-            </section>
+            </main>
         </div>
     )
 }
